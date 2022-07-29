fe528202a242a0c7c499551ca8799babda26640f
Kamila Koksharova <Kamilla_b_m@gmail.ru>
2022-07-29 16:25:00 +0500

    file

--- Angry_comments.py
+++ Angry_comments.py
@@ -3,7 +3,6 @@
 # TODO PRECISION > 0.95
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import nltk
 import string
 from nltk.corpus import stopwords
@@ -16,10 +15,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import PrecisionRecallDisplay
 from sklearn.metrics import precision_score, recall_score, precision_recall_curve
 from matplotlib import pyplot as plt
-from sklearn.metrics import plot_precision_recall_curve
 import numpy as np
 from sklearn.model_selection import GridSearchCV
-import matplotlib.pyplot as plt
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -28,18 +25,7 @@ data_list = pd.read_csv('./data/labeled.csv', sep=',')
 
 data_list['toxic'] = data_list['toxic'].apply(int)
 # .apply - к каждой строчке применяем одну и ту же функцию(приведение float к int)
-
-# print(data_list.shape)
-# print(data_list.head(5))
-# print(data_list['toxic'].value_counts())
-# print(*(i for i in data_list[data_list['toxic'] == 1]['comment'].head(6)))
-# print(*(i for i in data_list[data_list['toxic'] == 0]['comment'].head(5)))
-
 train_data_list, test_data_list = train_test_split(data_list, test_size=500)
-# print(test_data_list.shape)
-# print(test_data_list['toxic'].value_counts())
-# print(train_data_list['toxic'].value_counts())
-
 
 sentence_example = data_list.iloc[1]['comment']
 snowball = SnowballStemmer(language='russian')
@@ -60,28 +46,73 @@ features = vectorizer.fit_transform(train_data_list['comment'])
 model = LogisticRegression(random_state=0)
 model.fit(features, train_data_list['toxic'])
 
-model_pipline = Pipeline([
-    ('vectorizer', TfidfVectorizer(tokenizer=lambda x: tokenize_sentence(x, remove_stop_words=True))),
-    ('model', LogisticRegression(random_state=0))
-]
-)
-
-model_pipline.fit(train_data_list['comment'], train_data_list['toxic'])
+# model_pipline = Pipeline([
+#     ('vectorizer', TfidfVectorizer(tokenizer=lambda x: tokenize_sentence(x, remove_stop_words=True))),
+#     ('model', LogisticRegression(random_state=0))
+# ]
+# )
+#
+# model_pipline.fit(train_data_list['comment'], train_data_list['toxic'])
 # print(model_pipline.fit(train_data_list['comment'], train_data_list['toxic']))
 
-# text = ""
-# while text != '0':
-#     text = input('Введите комментарий: ')
-#     check = model_pipline.predict([text])
-#     if 1 in check:
-#         print('Это токсичный комментарий')
-#     else:
-#         print('Это нейтральный комментарий')
 
-print(precision_score(y_true=test_data_list['toxic'], y_pred=model_pipline.predict(test_data_list['comment'])))
-print(recall_score(y_true=test_data_list['toxic'], y_pred=model_pipline.predict(test_data_list['comment'])))
-prec, rec, thresholds = precision_recall_curve(y_true=test_data_list['toxic'],
-                                               probas_pred=model_pipline.predict_proba(test_data_list['comment'])[:, 1])
-disp = PrecisionRecallDisplay.from_estimator(estimator=model_pipline, X=test_data_list['comment'], y=test_data_list['toxic'])
-disp.plot()
+# prec, rec, thresholds = precision_recall_curve(y_true=test_data_list['toxic'],
+#                                                probas_pred=model_pipline.predict_proba(test_data_list['comment'])[:, 1])
+#
+# disp = PrecisionRecallDisplay.from_estimator(estimator=model_pipline, X=test_data_list['comment'],
+#                                              y=test_data_list['toxic'])
+# disp.plot()
+# plt.show()
+# array_np = np.where(prec > 0.95)
+#
+# print(array_np)
+# print(precision_score(y_true=test_data_list['toxic'],
+#                       y_pred=model_pipline.predict_proba(test_data_list['comment'])[:, 1] > thresholds[444]))
+# print(recall_score(y_true=test_data_list['toxic'],
+#                    y_pred=model_pipline.predict_proba(test_data_list['comment'])[:, 1] > thresholds[444]))
+
+
+model_pipline_c_10 = Pipeline([
+    ('vectorizer', TfidfVectorizer(tokenizer=lambda x: tokenize_sentence(x, remove_stop_words=True))),
+    ('model', LogisticRegression(random_state=0, C=10))
+]
+)
+model_pipline_c_10.fit(train_data_list['comment'], train_data_list['toxic'])
+
+prec_c_10, rec_c_10, thresholds_c_10 = precision_recall_curve(y_true=test_data_list['toxic'],
+                                                              probas_pred=model_pipline_c_10.predict_proba(
+                                                                  test_data_list['comment'])[:, 1])
+
+print(precision_score(y_true=test_data_list['toxic'],
+                      y_pred=model_pipline_c_10.predict_proba(test_data_list['comment'])[:, 1] > thresholds_c_10[444]))
+print(recall_score(y_true=test_data_list['toxic'],
+                   y_pred=model_pipline_c_10.predict_proba(test_data_list['comment'])[:, 1] > thresholds_c_10[444]))
+
+disp_c_10 = PrecisionRecallDisplay.from_estimator(estimator=model_pipline_c_10, X=test_data_list['comment'],
+                                                  y=test_data_list['toxic'])
+disp_c_10.plot()
 plt.show()
+array_np = np.where(prec_c_10 > 0.95)
+print(array_np)
+
+# grid_pipline = Pipeline([
+#     ('vectorizer', TfidfVectorizer(tokenizer=lambda x: tokenize_sentence(x, remove_stop_words=True))),
+#     ('model',
+#      GridSearchCV(
+#          LogisticRegression(random_state=0),
+#          param_grid={'C': [0.1, 1, 10]},
+#          cv=3,
+#          verbose=4
+#      )
+#      )
+# ])
+
+
+text = ""
+while text != '0':
+    text = input('Введите комментарий: ')
+    check = model_pipline_c_10.predict([text])
+    if 1 in check:
+        print('Это токсичный комментарий')
+    else:
+        print('Это нейтральный комментарий')
